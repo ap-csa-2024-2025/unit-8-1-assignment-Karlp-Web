fe0a052438d847cba178793a56d00bc15bef4a0a
Karlp-Web <karlenportillo1@gmail.com>
2025-03-06 19:02:10 +0000

    yomomma

--- Main.java
+++ Main.java
@@ -4,7 +4,49 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Test code here
+    int [] [] matrix = new int[3][7];
+
+    for (int r = 0; r < matrix.length; row++)
+    {
+      for (int c = 0; c < matrix[0].length; c++)
+      {
+        matrix [r][c] = (int) (math.random() *100);
+      }
+    }
+
+
+    for (int row = 0; row < matrix.length; row++)
+    {
+      int [] arr = matric[row];
+      //unit 6 problem
+      for (int i = 0; i < arr.length; i++)
+      {
+        System.out.print(arr[i] + " ");
+      }
+      System.out.println();
+    }
+
+    //Ap test will show it like this
+    for (int row = 0; row < matrix.legnth; row++)
+    {
+      for (int col = 0; matric[row].length; col++)
+      {
+        System.out.print(matric[row][col] + " ");
+      }
+      System.out.println();
+    }
+
+
+
+    //for each element 
+    for (int [] row: matrix)
+    {
+      for (int elem : row)
+      {
+        System.out.print(elem + " ");
+      }
+      System.out.println();
+    }
   }
 
 
